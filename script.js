4b20eaceb35cd9e2aefbd988d5831b41eeee0ff3
Anil Sapkota <45574896+anilsapkota@users.noreply.github.com>
2024-11-27 15:17:35 +0000

    Arguments in functions

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ info = {
   youtube: 'youtube.com/planetoftheweb',
   linkedin: 'linkedin.com/in/planetoftheweb'
 }
-function displayCard() {
+function displayCard(myObj) {
+    return (
+        `<strong>Name</strong>:${myObj.name}<br>
+        <strong>Twitter</strong>:${myObj.twitter}<br>`
+    )
 }
 $('#output').innerHTML = displayCard(info)
